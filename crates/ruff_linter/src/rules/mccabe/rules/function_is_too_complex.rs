06061c25c672f50b43b3e52a83af792271ca6bfb
Darius <darius@adroiti.com>
2025-05-16 18:25:08 +0300

    Complexity: do not count function definitions

--- crates/ruff_linter/src/rules/mccabe/rules/function_is_too_complex.rs
+++ crates/ruff_linter/src/rules/mccabe/rules/function_is_too_complex.rs
@@ -180,7 +180,6 @@ fn get_complexity_number(stmts: &[Stmt]) -> usize {
                 }
             }
             Stmt::FunctionDef(ast::StmtFunctionDef { body, .. }) => {
-                complexity += 1;
                 complexity += get_complexity_number(body);
             }
             Stmt::ClassDef(ast::StmtClassDef { body, .. }) => {
@@ -233,7 +232,7 @@ def trivial():
     pass
 ";
         let stmts = parse_suite(source)?;
-        assert_eq!(get_complexity_number(&stmts), 1);
+        assert_eq!(get_complexity_number(&stmts), 0);
         Ok(())
     }
 
@@ -244,7 +243,7 @@ def expr_as_statement():
     0xF00D
 ";
         let stmts = parse_suite(source)?;
-        assert_eq!(get_complexity_number(&stmts), 1);
+        assert_eq!(get_complexity_number(&stmts), 0);
         Ok(())
     }
 
@@ -257,14 +256,14 @@ def sequential(n):
     return s
 ";
         let stmts = parse_suite(source)?;
-        assert_eq!(get_complexity_number(&stmts), 1);
+        assert_eq!(get_complexity_number(&stmts), 0);
         Ok(())
     }
 
     #[test]
     fn if_elif_else_dead_path() -> Result<()> {
         let source = r#"
-def if_elif_else_dead_path(n):      # +1
+def if_elif_else_dead_path(n):
     if n > 3:                       # +1
         return "bigger than three" 
     elif n > 4:                     # +1
@@ -273,14 +272,14 @@ def if_elif_else_dead_path(n):      # +1
         return "smaller than or equal to three"
 "#;
         let stmts = parse_suite(source)?;
-        assert_eq!(get_complexity_number(&stmts), 4);
+        assert_eq!(get_complexity_number(&stmts), 3);
         Ok(())
     }
 
     #[test]
     fn nested_ifs() -> Result<()> {
         let source = r#"
-def nested_ifs():                               # +1
+def nested_ifs():
     if n > 3:                                   # +1
         if n > 4:                               # +1
             return "bigger than four"
@@ -290,7 +289,7 @@ def nested_ifs():                               # +1
         return "smaller than or equal to three"
 "#;
         let stmts = parse_suite(source)?;
-        assert_eq!(get_complexity_number(&stmts), 5);
+        assert_eq!(get_complexity_number(&stmts), 4);
         Ok(())
     }
 
@@ -302,35 +301,35 @@ def for_loop():
         print(i)
 ";
         let stmts = parse_suite(source)?;
-        assert_eq!(get_complexity_number(&stmts), 2);
+        assert_eq!(get_complexity_number(&stmts), 1);
         Ok(())
     }
 
     #[test]
     fn for_else() -> Result<()> {
         let source = r"
-def for_else(mylist): # +1
+def for_else(mylist):
     for i in mylist:  # +1
         print(i)
     else:             # +1
         print(None)
 ";
         let stmts = parse_suite(source)?;
-        assert_eq!(get_complexity_number(&stmts), 3);
+        assert_eq!(get_complexity_number(&stmts), 2);
         Ok(())
     }
 
     #[test]
     fn recursive() -> Result<()> {
         let source = r"
-def recursive(n):       # +1
+def recursive(n):
     if n > 4:           # +1
         return f(n - 1)
     else:               # +1
         return n
 ";
         let stmts = parse_suite(source)?;
-        assert_eq!(get_complexity_number(&stmts), 3);
+        assert_eq!(get_complexity_number(&stmts), 2);
         Ok(())
     }
 
@@ -347,7 +346,7 @@ def nested_functions():
     a()
 ";
         let stmts = parse_suite(source)?;
-        assert_eq!(get_complexity_number(&stmts), 3);
+        assert_eq!(get_complexity_number(&stmts), 0);
         Ok(())
     }
 
@@ -364,7 +363,7 @@ def nested_try_finally():
         print(3)
 ";
         let stmts = parse_suite(source)?;
-        assert_eq!(get_complexity_number(&stmts), 1);
+        assert_eq!(get_complexity_number(&stmts), 0);
         Ok(())
     }
 
@@ -373,15 +372,15 @@ def nested_try_finally():
         let source = r"
 async def foobar(a, b, c):
     await whatever(a, b, c)
-    if await b:
+    if await b:             # +1
         pass
     async with c:
         pass
-    async for x in a:
+    async for x in a:       # +1
         pass
 ";
         let stmts = parse_suite(source)?;
-        assert_eq!(get_complexity_number(&stmts), 3);
+        assert_eq!(get_complexity_number(&stmts), 2);
         Ok(())
     }
 
@@ -392,7 +391,7 @@ def annotated_assign():
     x: Any = None
 ";
         let stmts = parse_suite(source)?;
-        assert_eq!(get_complexity_number(&stmts), 1);
+        assert_eq!(get_complexity_number(&stmts), 0);
         Ok(())
     }
 
@@ -401,7 +400,7 @@ def annotated_assign():
         let source = r"
 class Class:
     def handle(self, *args, **options):
-        if args:
+        if args:                        # +1
             return
 
         class ServiceProvider:
@@ -409,26 +408,13 @@ class Class:
                 pass
 
             def b(self, data):
-                if not args:
+                if not args:            # +1
                     pass
-
-        class Logger:
-            def c(*args, **kwargs):
-                pass
-
-            def error(self, message):
-                pass
-
-            def info(self, message):
-                pass
-
-            def exception(self):
-                pass
 
         return ServiceProvider(Logger())
 ";
         let stmts = parse_suite(source)?;
-        assert_eq!(get_complexity_number(&stmts), 9);
+        assert_eq!(get_complexity_number(&stmts), 2);
         Ok(())
     }
 
@@ -442,7 +428,7 @@ def process_detect_lines():
         pass
 ";
         let stmts = parse_suite(source)?;
-        assert_eq!(get_complexity_number(&stmts), 1);
+        assert_eq!(get_complexity_number(&stmts), 0);
         Ok(())
     }
 
@@ -457,7 +443,7 @@ def process_detect_lines():
             errors.append(f"Non-zero exit code {res}")
 "#;
         let stmts = parse_suite(source)?;
-        assert_eq!(get_complexity_number(&stmts), 2);
+        assert_eq!(get_complexity_number(&stmts), 1);
         Ok(())
     }
 
@@ -470,7 +456,7 @@ def with_lock():
             print('bar')
 ";
         let stmts = parse_suite(source)?;
-        assert_eq!(get_complexity_number(&stmts), 2);
+        assert_eq!(get_complexity_number(&stmts), 1);
         Ok(())
     }
 
@@ -485,7 +471,7 @@ def f():
             print('bar')
 ";
         let stmts = parse_suite(source)?;
-        assert_eq!(get_complexity_number(&stmts), 3);
+        assert_eq!(get_complexity_number(&stmts), 2);
         Ok(())
     }
 
@@ -504,7 +490,7 @@ def f():
             print('baz')
 ";
         let stmts = parse_suite(source)?;
-        assert_eq!(get_complexity_number(&stmts), 3);
+        assert_eq!(get_complexity_number(&stmts), 2);
         Ok(())
     }
 
@@ -519,7 +505,7 @@ def f():
             print(x)
 ";
         let stmts = parse_suite(source)?;
-        assert_eq!(get_complexity_number(&stmts), 3);
+        assert_eq!(get_complexity_number(&stmts), 2);
         Ok(())
     }
 
@@ -527,14 +513,14 @@ def f():
     fn match_case_catch_all_with_sequence() -> Result<()> {
         let source = r"
 def f():
-    match subject:
+    match subject:          # +1
         case 2:
             print('hello')
-        case 5 | _:
+        case 5 | _:         # +1 for _
             print(x)
 ";
         let stmts = parse_suite(source)?;
-        assert_eq!(get_complexity_number(&stmts), 3);
+        assert_eq!(get_complexity_number(&stmts), 2);
         Ok(())
     }
 
@@ -548,7 +534,7 @@ class C:
             pass
 ";
         let stmts = parse_suite(source)?;
-        assert_eq!(get_complexity_number(&stmts), 3);
+        assert_eq!(get_complexity_number(&stmts), 2);
         Ok(())
     }
 
@@ -562,7 +548,7 @@ class C:
             pass
 ";
         let stmts = parse_suite(source)?;
-        assert_eq!(get_complexity_number(&stmts), 4);
+        assert_eq!(get_complexity_number(&stmts), 3);
         Ok(())
     }    
 
@@ -571,12 +557,12 @@ class C:
         let source = r"
 class C:
     def M(self, a, b, c, d):
-        if (a and b) or (c and d):   #  +1 for if; +1 for and; +1 for or; +1 for and (new)
+        if (a and b) or (c and d):   # +1 for if; +1 for and; +1 for or; +1 for and (new)
             # no-op
             pass
 ";
         let stmts = parse_suite(source)?;
-        assert_eq!(get_complexity_number(&stmts), 5);
+        assert_eq!(get_complexity_number(&stmts), 4);
         Ok(())
     }
 
@@ -588,7 +574,7 @@ def test_while():
         pass
 ";
         let stmts = parse_suite(source)?;
-        assert_eq!(get_complexity_number(&stmts), 3);
+        assert_eq!(get_complexity_number(&stmts), 2);
         Ok(())
     } 
 
@@ -596,23 +582,9 @@ def test_while():
     fn match_with_guard_logical_expression() -> Result<()> {
         let source = r"
 def test_match(value):
-    match value:
-        case 1 if (a and b) or c:  # +1 for match, +1 for case, +1 for and, +1 for or
+    match value:                   # +1
+        case 1 if (a and b) or c:  # +1 for and, +1 for or
             pass
-";
-        let stmts = parse_suite(source)?;
-        assert_eq!(get_complexity_number(&stmts), 4);
-        Ok(())
-    }
-
-    #[test]
-    fn else_in_while() -> Result<()> {
-        let source = r"
-def while_else(condition): # +1
-    while condition:       # +1
-        print('in while')
-    else:                  # +1
-        print('in else')
 ";
         let stmts = parse_suite(source)?;
         assert_eq!(get_complexity_number(&stmts), 3);
@@ -620,9 +592,23 @@ def while_else(condition): # +1
     }
 
     #[test]
+    fn else_in_while() -> Result<()> {
+        let source = r"
+def while_else(condition):
+    while condition:       # +1
+        print('in while')
+    else:                  # +1
+        print('in else')
+";
+        let stmts = parse_suite(source)?;
+        assert_eq!(get_complexity_number(&stmts), 2);
+        Ok(())
+    }
+
+    #[test]
     fn else_in_try() -> Result<()> {
         let source = r"
-def try_else():   # +1
+def try_else():
     try:
         print(1)
     except TypeA: # +1
@@ -633,7 +619,7 @@ def try_else():   # +1
         print(4)
 ";
         let stmts = parse_suite(source)?;
-        assert_eq!(get_complexity_number(&stmts), 3);
+        assert_eq!(get_complexity_number(&stmts), 2);
         Ok(())
     }
 }
